c5cf06919d659dd9e3b20222462de63de2cd896a
eduardomarquez1 <eduardomarquez2576@gmail.com>
2026-01-31 23:19:34 -0300

    Condição if

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 40;
+console.log(`Sua velocidade atual é de ${velocidade}KM/h`)
+if (velocidade > 80){
+    console.log(`Você foi MULTADO POR EXCESSO DE VELOCIDADE!! `)
+}
+console.log(`Dirija com segurança!!`)
