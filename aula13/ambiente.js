422979b6e6e7deea36b5f7ab531e84e86c46bfd7
eduardomarquez1 <eduardomarquez2576@gmail.com>
2026-02-11 18:36:07 -0300

    Estrutura de Repetição com teste lógico
    Aula dedicada ao aprendizado do 'while' e 'do while'

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+var contador = 1;
+do {
+    console.log(`Dia ${contador}`);
+    contador++;
+}while (contador <= 7)
+
+
+/*while (contador <= 7) {
+    console.log(`Dia ${contador}`)
+    contador++
+}
+*/
