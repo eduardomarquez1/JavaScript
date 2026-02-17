d5b1b9b76b68cc41d4a50da3c4fe5c2ce72d9e27
eduardomarquez1 <eduardomarquez2576@gmail.com>
2026-02-16 21:18:52 -0300

    Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-window.alert('Olá');
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0) {
+        alert('Por Favor, Digite um número!')
+    }else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for (var c = 1; c <= 10; c++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
