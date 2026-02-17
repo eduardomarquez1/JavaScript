2c414c96dfd68ad5b18e1086d43899e5093ef14e
eduardomarquez1 <eduardomarquez2576@gmail.com>
2026-02-16 22:00:22 -0300

    Desafio Antes e Depois
    Desafio consiste em solicitar um número inteiro ao usuário (a partir de um prompt) e mostrar o antecessor e sucessor desse mesmo número (por meio de um alert)

---
+++ desafios/d003/default.js
@@ -0,0 +1,6 @@
+function antesdepois() {
+    var numero = parseInt(prompt("Digite um número:"));
+    var antecessor = numero - 1;
+    var sucessor = numero + 1;
+    alert(`Antes de ${numero}, temos o número ${antecessor}.\nDepois de ${numero}, temos o número ${sucessor}.`)
+}
